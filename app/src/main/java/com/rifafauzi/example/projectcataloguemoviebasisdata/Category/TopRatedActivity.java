9f9f334595592999fae251f74e7fe9fc1785f6f6
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2018-08-13 02:50:03 +0700

    tinggal menambahkan favorit activity

--- app/src/main/java/com/rifafauzi/example/projectcataloguemoviebasisdata/Category/TopRatedActivity.java
+++ app/src/main/java/com/rifafauzi/example/projectcataloguemoviebasisdata/Category/TopRatedActivity.java
@@ -1,24 +1,86 @@
 package com.rifafauzi.example.projectcataloguemoviebasisdata.Category;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
+import com.rifafauzi.example.projectcataloguemoviebasisdata.API.BaseApiService;
+import com.rifafauzi.example.projectcataloguemoviebasisdata.API.Server;
+import com.rifafauzi.example.projectcataloguemoviebasisdata.Adapter.MovieAdapter;
+import com.rifafauzi.example.projectcataloguemoviebasisdata.BuildConfig;
+import com.rifafauzi.example.projectcataloguemoviebasisdata.Entity.Movies;
+import com.rifafauzi.example.projectcataloguemoviebasisdata.Entity.ResponseMovies;
 import com.rifafauzi.example.projectcataloguemoviebasisdata.MainActivity;
 import com.rifafauzi.example.projectcataloguemoviebasisdata.R;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class TopRatedActivity extends AppCompatActivity {
+
+    RecyclerView rvMovies;
+    private MovieAdapter adapter;
+    List<Movies> listMovies = new ArrayList<>();
+    ProgressDialog loading;
+    BaseApiService apiService;
+
+    private final String api_key = BuildConfig.MOVIE_DB_API_KEY;
+    private final String language = "en-US";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_rated);
 
+        rvMovies = findViewById(R.id.rv_movies);
+
+        apiService = Server.getAPIService();
+
+        adapter = new MovieAdapter(getApplicationContext(), listMovies);
+
+        rvMovies.setHasFixedSize(true);
+        rvMovies.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        rvMovies.setAdapter(adapter);
+
+        refresh();
+
         //membuat back button toolbar
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    private void refresh(){
+        loading = ProgressDialog.show(this, null, "Please wait...", true, false);
+        apiService.getTopRatedMovies(api_key, language).enqueue(new Callback<ResponseMovies>() {
+            @Override
+            public void onResponse(Call<ResponseMovies> call, Response<ResponseMovies> response) {
+                if (response.isSuccessful()){
+                    loading.dismiss();
+                    listMovies = response.body().getMovies();
+                    rvMovies.setAdapter(new MovieAdapter(getApplicationContext(), listMovies));
+                    adapter.notifyDataSetChanged();
+                }
+                else {
+                    loading.dismiss();
+                    Toast.makeText(getApplicationContext(), "Failed to Fetch Data !", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseMovies> call, Throwable t) {
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), "Failed to Connect Internet !", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/rifafauzi/example/projectcataloguemoviebasisdata/MainActivity.java
+++ app/src/main/java/com/rifafauzi/example/projectcataloguemoviebasisdata/MainActivity.java
@@ -1,7 +1,12 @@
 package com.rifafauzi.example.projectcataloguemoviebasisdata;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,15 +15,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.rifafauzi.example.projectcataloguemoviebasisdata.API.BaseApiService;
+import com.rifafauzi.example.projectcataloguemoviebasisdata.API.Server;
+import com.rifafauzi.example.projectcataloguemoviebasisdata.Adapter.MovieAdapter;
+import com.rifafauzi.example.projectcataloguemoviebasisdata.Category.FavoriteActivity;
 import com.rifafauzi.example.projectcataloguemoviebasisdata.Category.MostPopularActivity;
 import com.rifafauzi.example.projectcataloguemoviebasisdata.Category.NowPlayingActivity;
 import com.rifafauzi.example.projectcataloguemoviebasisdata.Category.TopRatedActivity;
 import com.rifafauzi.example.projectcataloguemoviebasisdata.Category.UpComingActivity;
+import com.rifafauzi.example.projectcataloguemoviebasisdata.Entity.Movies;
+import com.rifafauzi.example.projectcataloguemoviebasisdata.Entity.ResponseMovies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    RecyclerView rvMovies;
+    private MovieAdapter adapter;
+    List<Movies> listMovies = new ArrayList<>();
+    ProgressDialog loading;
+    BaseApiService apiService;
+
+    private final String api_key = BuildConfig.MOVIE_DB_API_KEY;
+    private final String language = "en-US";
+    private final String sort_by = "popularity.desc";
+    private final String include_adult = "false";
+    private final String include_video = "false";
+    private final String page = "1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +59,17 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        rvMovies = findViewById(R.id.rv_movies);
+
+        apiService = Server.getAPIService();
+
+        adapter = new MovieAdapter(getApplicationContext(), listMovies);
+
+        rvMovies.setHasFixedSize(true);
+        rvMovies.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        rvMovies.setAdapter(adapter);
+
+        refresh();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -52,7 +95,76 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.search));
+        searchView.setQueryHint(getResources().getString(R.string.search));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchMovie(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (TextUtils.isEmpty(newText)){
+                    refresh();
+                }
+                return false;
+            }
+        });
+
         return true;
+    }
+
+    public void searchMovie(String keyword){
+        apiService.getSearchMovie(api_key, language, keyword, page, include_adult).enqueue(new Callback<ResponseMovies>() {
+            @Override
+            public void onResponse(Call<ResponseMovies> call, Response<ResponseMovies> response) {
+                if (response.isSuccessful()){
+                    listMovies = response.body().getMovies();
+
+                    rvMovies.setAdapter(new MovieAdapter(getApplicationContext(), listMovies));
+                    adapter.notifyDataSetChanged();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Failed to Fetch Data !", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseMovies> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Failed to Connect Internet !", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void refresh(){
+        loading = ProgressDialog.show(this, null, "Please wait...", true, false);
+
+        apiService.getAllMovies(api_key, language, sort_by, include_adult, include_video, page).enqueue(new Callback<ResponseMovies>() {
+            @Override
+            public void onResponse(Call<ResponseMovies> call, Response<ResponseMovies> response) {
+                if (response.isSuccessful()){
+                    loading.dismiss();
+                    listMovies = response.body().getMovies();
+                    rvMovies.setAdapter(new MovieAdapter(getApplicationContext(), listMovies));
+                    adapter.notifyDataSetChanged();
+                }
+                else {
+                    loading.dismiss();
+                    Toast.makeText(getApplicationContext(), "Failed to Fetch Data !", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseMovies> call, Throwable t) {
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), "Failed to Connect Internet !", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -93,7 +205,7 @@ public class MainActivity extends AppCompatActivity
             startActivity(up);
             Toast.makeText(getApplicationContext(), "Show Up Coming Movies", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.favorite) {
-            Intent fav = new Intent(getApplicationContext(), UpComingActivity.class);
+            Intent fav = new Intent(getApplicationContext(), FavoriteActivity.class);
             startActivity(fav);
             Toast.makeText(getApplicationContext(), "Show Favorite Movies", Toast.LENGTH_SHORT).show();
         }
