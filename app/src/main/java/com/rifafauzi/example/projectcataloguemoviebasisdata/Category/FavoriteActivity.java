bc17c1154d9e43155094512bd0fe6e668ec515d3
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2020-01-20 10:35:35 +0700

    refactoring to android X

--- app/src/main/java/com/rifafauzi/example/projectcataloguemoviebasisdata/Category/FavoriteActivity.java
+++ app/src/main/java/com/rifafauzi/example/projectcataloguemoviebasisdata/Category/FavoriteActivity.java
@@ -2,11 +2,11 @@ package com.rifafauzi.example.projectcataloguemoviebasisdata.Category;
 
 import android.database.Cursor;
 import android.os.AsyncTask;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
--- favoritemovie/src/main/java/com/rifafauzi/example/favoritemovie/Adapter/MovieAdapter.java
+++ favoritemovie/src/main/java/com/rifafauzi/example/favoritemovie/Adapter/MovieAdapter.java
@@ -3,7 +3,7 @@ package com.rifafauzi.example.favoritemovie.Adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.support.v7.widget.CardView;
+import androidx.cardview.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
